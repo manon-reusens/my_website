28f832a0dc39bfe6097af362c6f12d624a55a67f
Manon Reusens <u0146965@kuleuven.be>
2026-02-21 21:40:25 +0100

    fix teaching

--- react-website/src/pages/Teaching.tsx
+++ react-website/src/pages/Teaching.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, Tag, Row, Col, Typography, Space, Divider } from 'antd';
 import { TeachingAPI } from '../data/teaching';
+import './Teaching.css';
 
 const { Title, Paragraph, Text } = Typography;
 
@@ -8,32 +9,32 @@ const Teaching: React.FC = () => {
   const { current, guestLectures, assistantships } = TeachingAPI.getAll();
 
   return (
-    <Space direction="vertical" size="large" style={{ width: '100%', maxWidth: 1200 }}>
-      <Space direction="vertical" size="small">
+    <Space orientation="vertical" size="large" style={{ width: '100%', maxWidth: 1200 }}>
+      <Space orientation="vertical" size="small">
         <Title level={2}>Teaching</Title>
         <Paragraph type="secondary">Courses, guest lectures, and teaching assistantships</Paragraph>
       </Space>
 
       {/* Current Teaching */}
       {current.length > 0 && (
-        <Space direction="vertical" size="middle" style={{ width: '100%' }}>
+        <Space orientation="vertical" size="middle" style={{ width: '100%' }}>
           <Divider>
             <Title level={3} style={{ margin: 0 }}>Current Teaching</Title>
           </Divider>
           <Row gutter={[16, 16]}>
             {current.map((teaching) => (
               <Col key={teaching.id} xs={24}>
-                <Card>
-                  <Space direction="vertical" size="small" style={{ width: '100%' }}>
+                <Card className='teaching-card' hoverable>
+                  <Space orientation="vertical" size="small" style={{ width: '100%' }}>
                     <Space wrap>
-                      <Tag color="green">{teaching.role}</Tag>
+                      <Tag className='tag tag-primary'>{teaching.role}</Tag>
                       <Tag color="default">{teaching.level}</Tag>
                       <Text type="secondary">
                         {teaching.startYear}–{teaching.endYear || 'present'}
                       </Text>
                     </Space>
                     <Title level={4} style={{ margin: 0 }}>{teaching.course}</Title>
-                    <Text>{teaching.institution}</Text>
+                    <Tag>{teaching.institution}</Tag>
                     <Text type="secondary">
                       {teaching.ects && `${teaching.ects} ECTS`}
                       {teaching.students && ` • ${teaching.students} students`}
@@ -43,7 +44,7 @@ const Teaching: React.FC = () => {
                     {teaching.responsibilities && teaching.responsibilities.length > 0 && (
                       <div>
                         <Text strong>Responsibilities:</Text>
-                        <ul style={{ marginTop: 8 }}>
+                        <ul style={{ marginTop: 8, marginLeft: 20 }}>
                           {teaching.responsibilities.map((resp, idx) => (
                             <li key={idx}>{resp}</li>
                           ))}
@@ -66,17 +67,17 @@ const Teaching: React.FC = () => {
 
       {/* Guest Lectures */}
       {guestLectures.length > 0 && (
-        <Space direction="vertical" size="middle" style={{ width: '100%' }}>
+        <Space orientation="vertical" size="middle" style={{ width: '100%' }}>
           <Divider>
             <Title level={3} style={{ margin: 0 }}>Guest Lectures</Title>
           </Divider>
           <Row gutter={[16, 16]}>
             {guestLectures.map((lecture) => (
               <Col key={lecture.id} xs={24} md={12}>
-                <Card>
-                  <Space direction="vertical" size="small" style={{ width: '100%' }}>
+                <Card className='teaching-card' size="small" hoverable>
+                  <Space orientation="vertical" size="small" style={{ width: '100%' }}>
                     <Space>
-                      <Tag color="orange">Guest Lecture</Tag>
+                      <Tag className='tag tag-secondary'>Guest Lecture</Tag>
                       <Text type="secondary">{lecture.year}</Text>
                     </Space>
                     <Title level={5} style={{ margin: 0 }}>{lecture.course}</Title>
@@ -104,17 +105,17 @@ const Teaching: React.FC = () => {
 
       {/* Teaching Assistantships */}
       {assistantships.length > 0 && (
-        <Space direction="vertical" size="middle" style={{ width: '100%' }}>
+        <Space orientation="vertical" size="middle" style={{ width: '100%' }}>
           <Divider>
             <Title level={3} style={{ margin: 0 }}>Teaching Assistantships</Title>
           </Divider>
           <Row gutter={[16, 16]}>
             {assistantships.map((assistant) => (
               <Col key={assistant.id} xs={24} md={12}>
-                <Card>
-                  <Space direction="vertical" size="small" style={{ width: '100%' }}>
+                <Card className='teaching-card' size="small" hoverable>
+                  <Space orientation="vertical" size="small" style={{ width: '100%' }}>
                     <Space>
-                      <Tag>Teaching Assistant</Tag>
+                      <Tag className='tag tag-tertiary'>Teaching Assistant</Tag>
                       <Text type="secondary">
                         {assistant.startYear}–{assistant.endYear}
                       </Text>
