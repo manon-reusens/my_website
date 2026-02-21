15bbedf548185918630b68b3a21d24b36425a654
Manon Reusens <u0146965@kuleuven.be>
2026-02-21 21:30:48 +0100

    fix talks

--- react-website/src/pages/Talks.tsx
+++ react-website/src/pages/Talks.tsx
@@ -1,33 +1,68 @@
 import React from 'react';
-import { Card, Tag, Row, Col, Typography, Space, Divider } from 'antd';
+import { Card, Tag, Row, Col, Typography, Space, Divider, Button } from 'antd';
+import { websiteConfig } from '../data/config';
 import { TalksAPI } from '../data/talks';
 import { formatDate } from '../utils/helpers';
+import './Talks.css';
 
 const { Title, Paragraph, Text } = Typography;
 
 const Talks: React.FC = () => {
+  const { personal, keynote } = websiteConfig;
   const groupedByYear = TalksAPI.groupByYear({ excludeIds: ['custom-topics'] });
   const years = Object.keys(groupedByYear).sort((a, b) => Number(b) - Number(a));
 
   return (
-    <Space direction="vertical" size="large" style={{ width: '100%', maxWidth: 1200 }}>
-      <Space direction="vertical" size="small">
+    <Space orientation="vertical" size="large" style={{ width: '100%', maxWidth: 1200 }}>
+      <Space orientation="vertical" size="small">
         <Title level={2}>Talks & Presentations</Title>
         <Paragraph type="secondary">Keynotes, invited talks, and conference presentations</Paragraph>
       </Space>
 
+       {/* Keynote Section */}
+      {keynote.enabled && (
+        <Card className='keynote-hero'>
+          <Space orientation="vertical" size="middle" style={{ width: '100%' }}>
+            <Title level={2}>{keynote.headline}</Title>
+            <Paragraph>{keynote.description}</Paragraph>
+            <Row gutter={[16, 16]}>
+              {keynote.topics.map((topic, idx) => (
+                <Col key={idx} xs={24} sm={12} lg={6}>
+                  <Card className='keynote-card' size="small" hoverable>
+                    <Space orientation="vertical" size="small" style={{ width: '100%', textAlign: 'center' }}>
+                      <Text style={{ fontSize: '2em' }}>{topic.icon}</Text>
+                      <Title level={5} style={{ margin: 0 }}>{topic.title}</Title>
+                      <Text type="secondary">{topic.audience}</Text>
+                    </Space>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+            <div className="keynote-cta">
+              <Button type="primary" size="large" href={`mailto:${personal.contact.email}`}>
+              {keynote.contactButtonText}
+              </Button>
+            </div>
+          </Space>
+        </Card>
+      )}
+      <Divider />
+      <Space orientation="vertical" size="middle" style={{ width: '100%' }}>
+        <Title level={2}>Past Talks</Title>
+        </Space>
+
       {years.map((year) => (
-        <Space key={year} direction="vertical" size="middle" style={{ width: '100%' }}>
+        <Space key={year} orientation="vertical" size="middle" style={{ width: '100%' }}>
           <Divider>
             <Title level={3} style={{ margin: 0 }}>{year}</Title>
           </Divider>
           <Row gutter={[16, 16]}>
             {groupedByYear[year].map((talk) => (
               <Col key={talk.id} xs={24} lg={12}>
-                <Card hoverable>
-                  <Space direction="vertical" size="small" style={{ width: '100%' }}>
+                <Card className="publication-card" hoverable>
+                  <Space orientation="vertical" size="small" style={{ width: '100%' }}>
                     <Space>
-                      <Tag color={talk.type === 'Keynote' ? 'orange' : 'green'}>{talk.type}</Tag>
+                      <Tag className={talk.type === 'Keynote' || talk.type === 'Invited Talk' ? 'tag tag-secondary' : 'tag tag-primary'}>{talk.type}</Tag>
                       <Text type="secondary">{formatDate(talk.date, true)}</Text>
                     </Space>
                     <Title level={5} style={{ margin: 0 }}>{talk.title}</Title>
